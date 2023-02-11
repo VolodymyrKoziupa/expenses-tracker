29054cc8a3596739dd73877b70e5e5ac3be41607
unknown <Volodymyr.koziupa@gmail.com>
2023-02-11 16:51:48 +0200

    fix calendar

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -11,11 +11,11 @@ const ExpensesChart = props => {
         {label: 'May', value: 0},
         {label: 'Jun', value: 0},
         {label: 'Jul', value: 0},
-        {label: 'jan', value: 0},
-        {label: 'jan', value: 0},
-        {label: 'jan', value: 0},
-        {label: 'jan', value: 0},
-        {label: 'jan', value: 0}, 
+        {label: 'Aug', value: 0},
+        {label: 'Sep', value: 0},
+        {label: 'Oct', value: 0},
+        {label: 'Nov', value: 0},
+        {label: 'Dec', value: 0}, 
     ];
 
     for( const expense of props.expenses) {
